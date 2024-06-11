03af1bcbe4a8e06195845acc24823eae746508b4
Sam <samuelkebbell@gmail.com>
2024-06-11 14:45:38 +0100

    UPDATES

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -7,6 +7,7 @@ let firstName = "Sam"
 console.log(firstName) 
 */
 
+/* 
 true;
 console.log(true)
 
@@ -29,3 +30,39 @@ year = 1997;
 console.log(typeof year);
 
 console.log(typeof null);
+
+let age = 27;
+age = 31;
+
+const birthYear = 1997;
+// birthYear = 1998;
+
+// const job; (requires a value)
+*/
+
+const ageSam = 2024 - 1997;
+const agePaul = 2024 - 1967;
+console.log(agePaul, ageSam);
+
+console.log(agePaul / 2, ageSam * 2);
+console.log(agePaul / 2, ageSam ** 2);
+// ** means to the power of so 27 * 2 * 2
+
+const firstName = 'Sam'
+const lastName = 'Kebbell'
+console.log(firstName + ' ' + lastName)
+
+// Assignment Operators
+let x = 10 + 5; //15
+console.log(x);
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x ++; // x = x + 1 = 101
+x --; // x = x - 1 = 100
+x --; // x = x - 1 = 99
+console.log(x)
+
+// Comparison Operators
+console.log(agePaul > ageSam); // return an answer as a boolean (asking a question)
+console.log(ageSam >= 18); // >= Greater or equal too
+
