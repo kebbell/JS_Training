a5d7ce1dce37707f230608e323576459c2905ae8
Sam <samuelkebbell@gmail.com>
2024-06-11 14:22:46 +0100

    UPDATES

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,31 @@
+/* 
 let js = 'amazing';
-console.log(40 + 8 + 23 - 10)
+// console.log(40 + 8 + 23 - 10)
 
+// Variable name
 let firstName = "Sam"
-console.log(firstName)
+console.log(firstName) 
+*/
+
+true;
+console.log(true)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 21);
+// console.log(typeof 'Sam');
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(typeof year);
+
+console.log(typeof null);
