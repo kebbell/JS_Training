fc7e3ce4091cf81b2d7261b3491e23fe8233d630
Sam <samuelkebbell@gmail.com>
2024-06-12 15:12:07 +0100

    UPDATES

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -90,6 +90,161 @@ console.log(x, y);
 const averageAge = (agePaul + ageSam) / 2
 console.log(agePaul, ageSam, averageAge); // like maths () are always first
 */
+///////////////////////////////////////////////////////////////////////////////
 
+// CODING CHALLENGES 
 // FIRST TEST
 
+/*
+// Test 1
+let marksWeight = 78;
+let johnsWeight = 92;
+
+let marksHeight = 1.69;
+let johnsHeight = 1.95;
+
+marksBMI = marksWeight / (marksHeight * marksHeight)
+johnsBMI = johnsWeight / (johnsHeight * johnsHeight)
+console.log(marksBMI, johnsBMI)
+
+let marksHigherBMI = (marksBMI > johnsBMI)
+console.log(marksHigherBMI)
+*/
+
+
+/*
+// Test 2
+let marksWeight = 95;
+let johnsWeight = 85;
+
+let marksHeight = 1.88;
+let johnsHeight = 1.76;
+
+x = marksWeight / (marksHeight * marksHeight)
+y = johnsWeight / (johnsHeight * johnsHeight)
+console.log(x, y)
+
+let marksHigherBMI = (x > y)
+console.log(marksHigherBMI)
+*/
+
+/*
+const firstName = "Sam";
+const job = "Driver";
+const birthYear = "1997";
+const year = "2024";
+
+const sam = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!'
+
+console.log(sam)
+
+const samNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+
+console.log(samNew)
+
+console.log(`Just a regular string...`)
+
+console.log(`string
+on
+new
+lines`) // Enter for a new line
+*/
+
+/*
+const age = 12;
+
+if (age >= 18) {
+  console.log('Sam can start driving 🤪'); // CMD + CNTRL + SPACE for emojis
+}
+else {
+  const yearsLeft = 18 - age;
+  console.log(`Sam has ${yearsLeft} years until he can drive`)
+}
+
+// CONTROLLED STRUCTURE
+
+if() {
+
+} else {
+
+}
+
+
+const birthYear = 1997;
+let century;
+
+if(birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century)
+*/
+
+///////////////////////////////////////////////////////
+// CODING CHALLENGE
+
+// TEST 2
+/*
+let marksWeight = 78;
+let johnsWeight = 92;
+
+let marksHeight = 1.69;
+let johnsHeight = 1.95;
+
+marksBMI = marksWeight / (marksHeight * marksHeight)
+johnsBMI = johnsWeight / (johnsHeight * johnsHeight)
+
+let marksHigherBMI = (marksBMI > johnsBMI)
+
+//
+let bmi;
+
+if (marksBMI > johnsBMI) {
+  bmi = `Marks BMI (${marksBMI}) is higher than Johns (${johnsBMI})`
+} else {
+  bmi =`Johns BMI (${johnsBMI}) is higher than Marks (${marksBMI})`
+};
+
+console.log(bmi);
+
+/*
+let marksWeight = 95;
+let johnsWeight = 85;
+
+let marksHeight = 1.88;
+let johnsHeight = 1.76;
+
+x = marksWeight / (marksHeight * marksHeight)
+y = johnsWeight / (johnsHeight * johnsHeight)
+
+let marksHigherBMI = (x > y)
+
+//
+let bmi;
+
+
+if (marksBMI > johnsBMI) {
+  bmi = `Marks BMI (${marksBMI}) is higher than Johns (${johnsBMI})`
+} else {
+  bmi =`Johns BMI (${johnsBMI}) is higher than Marks (${marksBMI})`
+};
+
+console.log(bmi);
+*/
+
+// TYPE CONNVERSION
+/*
+const inputYear = '1997';
+console.log(Number(inputYear), inputYear); // 'Number' converts string to number (start with capital)
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Sam')); // NaN 'not a number' (invalid number)
+
+console.log(String(23)); // 'String' converts number to string (start with capital)
+*/
+
+// TYPE COERCION (JS converting for me)
+
+console.log('I am' + 27 + ' years old') // the + does the coercion for me (+ is always number to string)
+console.log('23' - '12') // the - does the coercion for me (- is always string to number)
