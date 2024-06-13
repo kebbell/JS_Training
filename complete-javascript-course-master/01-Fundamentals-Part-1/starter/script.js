c6986535f3d71a9f2f6d2000e475c41fa1bfdb22
Sam <samuelkebbell@gmail.com>
2024-06-13 15:10:27 +0100

    UPDATES

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -342,4 +342,47 @@ if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
 */
 
 // SWITCH STATEMENT L23
+/*
+const day = 'SUNDAY';
+
+switch(day) {
+  case 'Monday':
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'Tuesday':
+    console.log ('Prepare theory vid');
+    break;
+  case 'Wednesday':
+  case 'Thursday':
+    console.log('Write code examples');
+    break;
+  case 'Friday':
+    console.log('Record videos');
+    break;
+  case 'Saturday':
+  case 'Sunday':
+    console.log('Enjoy the weekend');
+    break;
+  default:
+    console.log('Not a day of the week!');
+}
+
+// USING THE SWITCH HELPS AVOID REPETITIVE CODE (LIKE BELOW)
+
+if (day === 'Monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'Tuesday') {
+  console.log ('Prepare theory vid');
+} else if (day === 'Wednesday' || day === 'Thursday') {
+  console.log('Write code examples');
+} else if (day ==='Friday') {
+  console.log('Record videos');
+} else if (day === 'Saturday' || day === 'Sunday') {
+  console.log('Enjoy the weekend');
+} else {    
+  console.log('Not a day of the week!');
+}
+*/
 
