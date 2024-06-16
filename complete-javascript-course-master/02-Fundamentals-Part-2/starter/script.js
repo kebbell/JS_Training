4b6324c3928d19d4ad0a86f743d4fcbbaeb57bc8
Sam <samuelkebbell@gmail.com>
2024-06-16 09:47:47 +0100

    UPDATES

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,33 @@
 'use strict'; // SHOULD ALWAYS USE!! - FORBIDS YOU DOING CERTAIN THINGS & GIVES VISUAL ERRORS THAT NORMAL JS WOULDNT
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive');
+*/
 
 /*
 const interface = 'Audio'; // RESERVED WORDS
 const private = 534; // RESERVED WORDS
 */
+
+// FUNCTIONS L30
+
+function logger() {
+  console.log('my name is Sam');
+}
+
+logger(); //CALLING, RUNNING OR ENVOKING THE FUNCTION (USING FUNCTION JUST DEFINED)
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice; 
+}
+
+fruitProcessor(5, 0); //ARGUMENTTS
+
