70545777476af37ed5e540c5bc7012569b29e28a
Sam <samuelkebbell@gmail.com>
2024-06-16 09:40:54 +0100

    UPDATES

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,12 @@
-'use strict';
+'use strict'; // SHOULD ALWAYS USE!! - FORBIDS YOU DOING CERTAIN THINGS & GIVES VISUAL ERRORS THAT NORMAL JS WOULDNT
 
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+/*
+const interface = 'Audio'; // RESERVED WORDS
+const private = 534; // RESERVED WORDS
+*/
