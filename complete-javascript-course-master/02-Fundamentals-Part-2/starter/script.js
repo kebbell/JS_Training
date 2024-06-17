64ddfd31e83900da47dfa644aac1fedd16e6bf6e
Sam <samuelkebbell@gmail.com>
2024-06-17 14:21:05 +0100

    UPDATES

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -15,6 +15,7 @@ const private = 534; // RESERVED WORDS
 
 // FUNCTIONS L30
 
+/*
 function logger() {
   console.log('my name is Sam');
 }
@@ -33,4 +34,40 @@ console.log(appleJuice)
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice)
+*/
 
+// FUNCTIONS - DECLARATION & EXPRESSIONS
+
+// DECLARATION FUNCTION //
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1997);
+console.log(age1);
+// ^^ DECLARATION FUNCTION ^^ //
+
+// EXPRESSION FUNCTION //
+const calcAge2 = function (birthYear) {
+ return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1997);
+
+console.log(age1, age2)
+// ^^ EXPRESSION FUNCTION ^^ //
+
+// ARROW FUNCTION // 
+const calcAge3 = birthYear => 2037 - birthYear // GOOD FOR ONE LINE FUNCTIONS
+const age3 = calcAge3(1997);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2024 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1997, 'Sam'));
+console.log(yearsUntilRetirement(1997, 'Sam'));
