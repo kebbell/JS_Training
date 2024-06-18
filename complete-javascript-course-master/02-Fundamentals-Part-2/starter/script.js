d40c6cd21418b401b3502a1c15f71ea9bf889c5e
Sam <samuelkebbell@gmail.com>
2024-06-18 14:36:04 +0100

    UPDATES

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -123,3 +123,18 @@ console.log(yearsUntilRetirement(1970, 'John'));
 
 // TEST 4
 
+const calcAverage = (a, b , c) => (a + b + c) / 2;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinnner = function(avgDolphins, avgKoalas) {
+  if (avgDolphins >= * 2 avgKoalas) {
+    `Dolphins Win!! ${avgDolphins} vs. ${avgKoalas}`
+  } else if (avgKoalas >= * 2 avgDolphins) {
+    `Koalas Win!! ${avgKoalas} vs. ${avgDolphins}`
+  } else {
+    console.log('No Winner');
+  }
+}
+checkWinnner(scoreDolphins, scoreKoalas);
