3717ed78bc74c4158db3812114655c9552ecf2a3
Sam <samuelkebbell@gmail.com>
2024-06-20 15:53:24 +0100

    UPDATES

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -244,9 +244,23 @@ if (friends.includes('Steven')) {
 // CODING CHALLENGE
 
 // TEST 5
-let bill = 125
 
-const calTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * .2 : bill * .15;
-  console.log(`The total bill is now ${bill}`);
-}
+// const calTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+// }
+
+// OR
+/*
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills, tips);
+ 
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
+*/
+
+// OJECTS L39
+
