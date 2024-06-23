429c833a3c28780e3717327f18075b4795d36547
Sam <samuelkebbell@gmail.com>
2024-06-23 14:21:37 +0100

    UPDATES

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -529,5 +529,14 @@ for (let exercise = 1; exercise < 4; exercise++) {
 
 let rep = 1;
 while (rep <= 10) {
-  console.log(`WHILE: Lifting weights reps ${rep}`); rep++;
+  // console.log(`WHILE: Lifting weights reps ${rep}`); rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('You rolled a 6 🎲')
 }
