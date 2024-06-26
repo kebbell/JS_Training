66684dbd33ba66ea77f213fdea5d3a98323dbb6f
Sam <samuelkebbell@gmail.com>
2024-06-26 15:42:19 +0100

    UPDATES

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -2,3 +2,10 @@
 
 console.log(document.querySelector('.message').textContent); // SHOWS THE TEXT AND NO SYNTAX
 
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
