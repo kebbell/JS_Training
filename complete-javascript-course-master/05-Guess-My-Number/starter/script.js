818d15c1e4e540a6fe92f4b83c114584da1bae7b
Sam <samuelkebbell@gmail.com>
2024-06-26 16:20:22 +0100

    UPDATES

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,14 +13,37 @@ console.log(document.querySelector('.guess').value);
 */
 
 
-const number = Math.trunc(Math.random()*20)+1;
-document.querySelector('.number').textContent = number
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber
+let score = 5;
+
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number (document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Input Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number ✅';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High ❌';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low ❌';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST';
+      document.querySelector('.score').textContent = 0;
+    }
+
   }
 })
-
+ 
