af1a116c95a8b8b83bc1f381be654addd42c112b
Sam <samuelkebbell@gmail.com>
2024-06-26 15:55:24 +0100

    UPDATES

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -12,10 +12,15 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+
+const number = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = number
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number (document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Input Number'
+    document.querySelector('.message').textContent = 'Input Number';
   }
 })
+
