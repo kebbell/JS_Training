289dc00c030f7ccbbbbf1e6364b941cfdfce7b9f
Sam <samuelkebbell@gmail.com>
2024-06-27 17:13:43 +0100

    UPDATES

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const closeBtn = document.querySelector('.close-modal');
+
+
+// BELOW FUNCTION WORKS WITH THE EVENT LISTENERS BELOW (BTNSOPENMODAL) //
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+// BELOW FUNCTION WORKS WITH THE EVENT LISTENERS BELOW (BTNCLOSEMODAL & OVERLAY) //
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener ('click', openModal);
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
