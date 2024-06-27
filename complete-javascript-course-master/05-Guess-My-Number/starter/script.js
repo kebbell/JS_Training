8d1d0816ba2d60fb9a25fe1eb6d495d272eb6644
Sam <samuelkebbell@gmail.com>
2024-06-27 15:56:30 +0100

    UPDATES

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -14,6 +14,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 5;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -31,6 +32,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('.number').style.width = '30rem';
 
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    
+  } else if (guess !== secretNumber) {
+      if (score > 1) {
+      document.querySelector('.message').textContent = guess > secretNumber ? 'Too High ❌' : 'Too Low ❌';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#fb1f1f';
+    }
+  }
+
+    /*
     // WHEN GUESS IS TOO HIGH
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -56,8 +76,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
       document.querySelector('body').style.backgroundColor = '#fb1f1f';
     }
-  }
+  }*/
 });
+
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 5;
@@ -70,4 +91,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('body').style.backgroundColor = '#222';
 });
-
