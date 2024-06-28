b079a820df80158158f077c7d310aaa8733e706e
Sam <samuelkebbell@gmail.com>
2024-06-28 17:00:57 +0100

    UPDATES

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // SELECTING ELEMENTS
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
 const score1 = document.querySelector('#score--0');
 // const score1 = document.getElementById('score--0') // SAME AS ABOVE
 const score2 = document.querySelector('#score--1');
@@ -16,7 +18,10 @@ score1.textContent = 0;
 score2.textContent = 0;
 diceL.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 
 // ROLLING DICE FUNCTIONALITY
 
@@ -32,10 +37,19 @@ btnRoll.addEventListener('click', function () {
   // 3. CHECK IF 1 IS ROLLED. IF TRUE, SWITTCH TO NEXT PLAYER
   if(dice !== 1) {
     // ADD DICE SCORE TO CURRENT SCORE
-    currentScore+= dice; // currentScore = currentScore + dice - is the same 
-    current1.textContent = currentScore; // CHANGE LATER
+    currentScore += dice; // currentScore = currentScore + dice - is the same 
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    // current1.textContent = currentScore; // ONLY CHANGES FOR CURRENT 1 BUT ABOVE DOES FOR WHICH EVER IS THE ACTIVE PLAYER
   } else {
     // SWITCH NEXT PLAYER
-
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player1.classList.toggle('player--active'); // TOGGLE WILL ADDS IF NOT THERE OR REMOVERS IF IT IS THERE
+    player2.classList.toggle('player--active'); // TOGGLE WILL ADDS IF NOT THERE OR REMOVERS IF IT IS THERE
   }
+});
+
+btnHold.addEventListener('click', function() {
+
 });
