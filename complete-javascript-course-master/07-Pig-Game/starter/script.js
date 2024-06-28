295d361dd9086e911b0c754306a5fe34e0df2d20
Sam <samuelkebbell@gmail.com>
2024-06-28 15:43:19 +0100

    UPDATES

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// SELECTING ELEMENTS
+const score1 = document.querySelector('#score--0');
+// const score1 = document.getElementById('score--0') // SAME AS ABOVE
+const score2 = document.querySelector('#score--1');
+const diceL = document.querySelector('.dice');
+
+score1.textContent = 0;
+score2.textContent = 0;
+diceL.classList.add('hidden');
