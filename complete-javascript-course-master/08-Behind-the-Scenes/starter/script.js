1dadd9daf7e6c70e5b6d9ed673758c9a86b3fe72
Sam <samuelkebbell@gmail.com>
2024-07-01 15:09:06 +0100

    UPDATES

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
-// SCOPE CHAIN
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
 
-// THIS KEYWORD
+  function printAge() {
+    const output = `${firstName} you're ${age} years old, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millennial, ${firstName}`;
+      console.log(str);
+    } else {const newStr =`${firstName} is not a millenial as he was born in the year ${birthYear}.`;
+  console.log(newStr);}
+  }
+  printAge(); // REMEMBER TO CALL THE FUNCTION!1
+
+  return age;
+}
+
+const firstName = 'Sam';
+calcAge(1997);
---
+++ complete-javascript-course-master/TEST.js
@@ -0,0 +1,23 @@
+// TEST QUESTIONS DONE RANDOMLY TO KEEP ME ALERT //
+
+// Question 1: Write a function named add that takes two arguments and returns their sum.
+function add(a, b) {
+  return a + b;
+}
+console.log(add(2 + 4));
+
+
+// Question 2: How does a function expression differ from a function declaration?
+// DECLARATION
+function norwich(a, b) {
+return a * b;
+}
+console.log(norwich('pukki' + ' ' + 'is' + ' ' + 'king'));
+
+// EXPRESSION
+const ncfc = function norwich(a, b) {
+  return a * b;
+}
+console.log(norwich('pukki' + ' ' + 'is' + ' ' + 'king'));
+
+// Question: What is an Immediately Invoked Function Expression (IIFE) and provide an example?
