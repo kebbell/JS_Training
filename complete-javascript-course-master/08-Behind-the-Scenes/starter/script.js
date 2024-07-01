7f72a2add3f4b8968c7c39021ba2b3dd253af05b
Sam <samuelkebbell@gmail.com>
2024-07-01 15:39:32 +0100

    UPDATES

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+/////////////////////////
+// FOR SCOPING
 function calcAge(birthYear) {
   const age = 2024 - birthYear;
 
@@ -10,8 +11,10 @@ function calcAge(birthYear) {
     if (birthYear >= 1981 && birthYear <= 1996) {
       const str = `Oh, and you're a millennial, ${firstName}`;
       console.log(str);
-    } else {const newStr =`${firstName} is not a millenial as he was born in the year ${birthYear}.`;
-  console.log(newStr);}
+    } else {
+      const newStr = `${firstName} is not a millenial as he was born in the year ${birthYear}.`;
+      console.log(newStr);
+    }
   }
   printAge(); // REMEMBER TO CALL THE FUNCTION!1
 
@@ -20,4 +23,34 @@ function calcAge(birthYear) {
 
 const firstName = 'Sam';
 calcAge(1997);
+
+///////////////////////////
+// FOR HOISTING
+
+// Hoisting Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Sam';
+let job = 'King';
+const year = 1997;
+
+// Hoisting Functions
+
+console.log(addDeclarationFunc(2, 3));
+console.log(addArrowFunc(2, 3));
+console.log(addExpressionFunc(2,  3));
+
+function addDeclarationFunc(a, b) {
+  return a + b;
+}
+// ONLY FUNCCTION DECLARATION CAN BE DEFINED BEFORE CALLING
+
+const addExpressionFunc = function (a, b) {
+  return a + b;
+};
+addExpressionFunc();
+
+const addArrowFunc = (a, b) => a + b;
 
