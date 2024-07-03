5bba0b5ad34085e1ef8be8d0940f49f3fac99d95
Sam <samuelkebbell@gmail.com>
2024-07-03 13:52:26 +0100

    UPDATES

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,18 +12,65 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[starterIndex]];
   },
+
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
 };
+
+const arr = [2, 4, 6];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// DESTRUCTURING THE ARRAY
+const [x, y, z] = arr; // MUCH SHORTER QUICKER WAY OF DOING THE ABOVE
+// REMEMBER TO DECLARE THE VARIABLE WITH CONST
+console.log(a, b, c, x, y, z);
+
+let [main, , secondary] = restaurant.categories; // , , = SKIPS
+console.log(main, secondary);
+
+// SWITCHING VARIABLES
+// IF YOU WANT TO SWITCH IT
+
+// CAN DO - WITHOUT DESTRUCTURING
+const temp = main;
+main = secondary;
+secondary = temp;
+// console.log(main, secondary);
+
+// DESTRUCTURING
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// RECIEVE 2 RETTURN VALUES FROM A FUNCTION
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// constt [i, ,j] = nested;
+// console.log(i, j);
+
+// DESTRUCTING WHEN NESTED
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// DEFAULT VALUES
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); // R HAS NO VALUE SO IT TAKES FROM THE R = 1
