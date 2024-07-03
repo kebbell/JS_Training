4d7d82776680096aea7393bd128c538ae050e8d6
Sam <samuelkebbell@gmail.com>
2024-07-03 16:48:29 +0100

    UPDATES

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -5,6 +5,22 @@
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,30 +28,27 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[starterIndex]];
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
   },
 
-  orderDelivery: function ({starterIndex, mainIndex, time, address}) {
-    console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[starterIndex]} will be delivered to ${address} at ${time}`);
-  }
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
+// SPREAD OPERATOR
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Ballamodha',
@@ -43,9 +56,53 @@ restaurant.orderDelivery({
   starterIndex: 2,
 }); // ARGUMENTS DONT HAVE TO BE IN ORDER
 
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnnocci', 'Ramen'];
+console.log(newMenu);
+
+// COPY ARRAY
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// JOIN 2 OR MORE ARRAYS
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+const extras = [...restaurant.categories, ...restaurant.name];
+
+console.log(menu, extras);
+
+// ITERABLES: ARRAYS, STRINGS, MAPS, SETS. NOT OBJECTS
+
+const ingredients = [
+  //   prompt('Let\'s make pasta! Ingredient 1?'),
+  //   prompt('Ingredient 2?'),
+  //   prompt('Ingredient 3?'),
+];
+
+// console.log(ingredients);
+
+// SPREAD OPERATOR
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients); // MUCH QUICKER THAN ORIGINAL CODE
+
+// OBJECTS
+
+const newRestaurant = { ...restaurant, founded: 1998, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
 //////////////////////////////////////////
 // DESTRUCTORING OBJECTS
-
+/*
 const { name, openingHours, categories } = restaurant;
 
 const {
