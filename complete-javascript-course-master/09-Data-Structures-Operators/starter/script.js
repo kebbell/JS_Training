ca285b1a07593b9e6ab506d0baef26a844939f16
Sam <samuelkebbell@gmail.com>
2024-07-06 11:51:07 +0100

    UPDATES

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,26 @@ const restaurant = {
   },
 };
 
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
 
 const game = {
   team1: 'Bayern Munich',
@@ -95,10 +115,36 @@ const game = {
   },
 };
 
-// 1.
+////////////////////////////////
+// CODING CHALLENGE 2
 
-for(const [i, player] of game.scored.entries()) 
-  console.log(`Goal ${i + 1}: ${player}`);
+// 1.
+for (const [i, players] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${players}`);
+}
+
+// 2.
+const odds = Object.values(game.odds);
+console.log(odds); // OBJECT.VALUES(GAME.ODDS) - SHOWS THE OBJECT VALUES
+let average = 0; // This line declares a variable average and initializes it to 0.
+for (const odd of odds) average += odd; // This is a for...of loop that iterates over each value in the odds array. For each odd value, it adds it to the average variable.
+average /= odds.length; // This line calculates the average by dividing the average variable by the length of the odds array.
+console.log(average);
+
+// 3.
+
+for(const [team, odd] of Object.entries(game.odds)) // This line starts a for...of loop that iterates over each key-value pair in the game.odds object. The Object.entries() method returns an array of a given object's own enumerable string-keyed property [key, value] pairs. 
+{
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+} 
+
+
+// console.log(`Odds of victory for ${game.team1}: ${game.odds.team1}`);
+// console.log(`Odd of draw: ${game.odds.x}`);
+// console.log(`Odds of victory for ${game.team2}: ${game.odds.team2}`);
+
+// 4 (BONUS)
 
 
 
