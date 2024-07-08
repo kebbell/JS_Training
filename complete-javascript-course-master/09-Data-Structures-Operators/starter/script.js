0a7a786448c23e1e940962321b30b27d279d11ee
Sam <samuelkebbell@gmail.com>
2024-07-08 14:51:17 +0100

    UPDATES

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,57 @@ const restaurant = {
   },
 };
 
+
+
+
+
+/*
+// MORE MAPS
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+console.log(answer);
+const answer = 3;
+
+console.log(question.get(answer === question.get('correct')));
+
+// for (const [key, value] of question) {
+//   if (typeof answer === true) {
+//     console.log(`${value}`);
+//   } else {
+//     console.log(`${value}`);
+//   }
+// }
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+// console.log(question.entries());
+
+/*
 // MAPS
 // Maps can be any type of key (string, number, boolean, object, etc), but an object is only a string key.
 
@@ -84,6 +135,7 @@ rest.set(document.querySelector('h1'), 'Heading');
 // rest.set([1, 2], 'Test');
 console.log(rest);
 console.log(rest.get(arr));
+
 
 /*
 // SETS
