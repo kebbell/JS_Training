2cbcdf29af4344c32e9de3c44fd0a25f72d039f8
Sam <samuelkebbell@gmail.com>
2024-07-10 14:43:36 +0100

    UPDATES

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Data needed for first part of the section
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
@@ -54,8 +53,16 @@ const restaurant = {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const getCode = str => str.slice(0,3).toUpperCase()
 
-
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(36);
+  console.log(output);
+}
 
 ///////////////////////////////////////
 // Coding Challenge #4
