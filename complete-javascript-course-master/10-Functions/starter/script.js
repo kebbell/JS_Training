7a63848ce22d246a3b082fb198e3befff4c40fec
Sam <samuelkebbell@gmail.com>
2024-07-11 14:48:15 +0100

    UPDATES

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -57,3 +57,35 @@ checkIn(flight, sam)
 */
 
 
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+}
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str} `);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+
+}
+
+// JS uses callbacks all the time
+transformer('Sam is working on Javascript', upperFirstWord)
+
+transformer('JavaScript is the best!', oneWord)
+
+const high5 = function () {
+  console.log('👋');
+}
+document.body.addEventListener('click', high5)
+
+['Jonas', 'Martha', 'Adam'].forEach(high5)
+
+
+//
+
