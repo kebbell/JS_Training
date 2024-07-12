b30ce09be75057f2fcaf096071bbe5162ebf7566
Sam <samuelkebbell@gmail.com>
2024-07-12 15:46:48 +0100

    UPDATES

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -243,7 +243,7 @@ const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section 😃
-  answers: new Array(4).fill(0),
+  answers: new Array(4).fill(5),
 };
 
 // 1
