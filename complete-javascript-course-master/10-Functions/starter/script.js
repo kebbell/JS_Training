5e9b81c8b7b36f1a6642cb30f166ceac6a54f1e4
Sam <samuelkebbell@gmail.com>
2024-07-15 14:49:28 +0100

    UPDATES

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -310,10 +310,13 @@ document
 
 */
 
+/*
+
 // MORE CLOSURES
 // CLOSURES BASIC MEANING - YOU CAN USE VARIABLES THAT HAVE ALREADY BEEN DEFINED IN THE OUTER SCOPE THAT SEEM TO HAVE NO ACCESS.
 // CLOSURES ARE THE BRIDGING GAP BETWEEN THE GLOBAL SCOPE
 
+// Example 1
 let f;
 const g = function () {
   const a = 23;
@@ -323,5 +326,41 @@ const g = function () {
   };
 };
 
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
 g();
 f();
+
+console.dir(f);
+// Re-assigning f function
+h();
+f();
+
+console.dir(f);
+//  ^^ CLOSURES RE-ASSIGNED OVER THE NEW VARIABLE ^^ //
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
+
+*/
+
+/////////////////////////////////////////////////////
+// Coding Challenge #4
+
+
