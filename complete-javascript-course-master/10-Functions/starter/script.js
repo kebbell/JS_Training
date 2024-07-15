1e60dd35fcf4d15a1c3f2e84aff77d8199ca2a03
Sam <samuelkebbell@gmail.com>
2024-07-15 15:39:33 +0100

    UPDATES

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -377,4 +377,9 @@ GOOD LUCK 😀
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = header.style.color === 'red' ? 'blue' : 'red';
+  });
 })();
+
