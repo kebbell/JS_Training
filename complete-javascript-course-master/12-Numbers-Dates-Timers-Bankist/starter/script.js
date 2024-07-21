5411fe451df07c270dd559f01cb6d41e86418123
Sam <samuelkebbell@gmail.com>
2024-07-21 10:16:10 +0100

    UPDATES

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -370,7 +370,7 @@ movements.forEach(function (mov, i, arr) {
 //   console.log(`${key}: ${value}`); // KEY DOESN'T EXIST IN SET
 //   console.log(currenciesUnique);
 
-
+/*
 console.log(Number('23'));
 console.log(+'23'); // SAME AS ABOVE
 // + is the same as Number
@@ -402,8 +402,8 @@ console.log(Number.isFinite(23 / 0));
 
 console.log(Number.isInteger('20'));
 console.log(Number.isInteger(23.0));
-
-
+*/
+/*
 // TRUTHY AND FALSY VALUES
 // 5 FALSY VALUES: 0, '', undefined, null, NaN
 console.log(Boolean(0));
@@ -411,3 +411,50 @@ console.log(Boolean(undefined));
 console.log(Boolean('Jonas'));
 console.log(Boolean({}));
 console.log(Boolean(''));
+*/
+
+// MATHS AND ROUNDING
+
+console.log(Math.sqrt(25)); // SQUARE ROOT
+console.log(25 ** (1 / 2)); // SQUARE ROOT
+console.log(8 ** (1 / 3)); // CUBIC ROOT
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2)); // WILL RETURN 23 AS MAX IS A STRING, TYPE COERCION
+console.log(Math.max(5, 18, '23px', 11, 2)); // PARSING WILL FAIL AS '23px' IS NOT A NUMBER
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0...(max - min) -> min...max
+// console.log(randomInt(10, 20));
+
+// Rounding integers
+
+console.log(Math.trunc(23.3)); // Math.trunc RETURNS THE INTEGER PART OF A NUMBER (REMOVES DECIMALS)
+
+console.log(Math.round(23.3)); // ROUNDS THE NUMBER DOWN
+console.log(Math.round(23.9)); // ROUNDS THE NUMBER DOWN
+
+console.log(Math.ceil(23.3)); // ROUNDS THE NUMBER UP
+console.log(Math.ceil(23.9)); // ROUNDS THE NUMBER UP
+
+console.log(Math.floor(23.3)); // ROUNDS THE NUMBER DOWN
+console.log(Math.floor('23.9')); // ROUNDS THE NUMBER DOWN
+// ALL WORK WITH COERCION
+
+console.log(Math.trunc(-23.3)); 
+console.log(Math.floor(-23.3)); // FLOOR WORKS CORRECTLY WITH NEGATIVE NUMBERS
+console.log(Math.ceil(-23.3)); 
+
+// Rounding decimals
+
+console.log((2.7).toFixed(0)); // RETURNS A STRING
+console.log((2.7).toFixed(3)); // RETURNS A STRING
+console.log(+(2.345).toFixed(2)); // + CONVERTS TO A NUMBER
+
