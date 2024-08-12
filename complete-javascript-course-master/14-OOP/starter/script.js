42aa2b2875a466ffda2ce9957eff48533f6d00eb
Sam <samuelkebbell@gmail.com>
2024-08-12 15:05:01 +0100

    UPDATES

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Player = function (name, marker) {
+  this.name = name;
+  this.marker = marker;
+};  
+
+Player.prototype.getMarker = function () {
+  return this.marker;
+}
+
+const player1 = new Player('player1', 'X');
+const player2 = new Player('player2', 'O');
+
+console.log(player1, player2)
