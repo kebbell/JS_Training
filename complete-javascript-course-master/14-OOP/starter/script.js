fe95c9401711ea9b7fd102cb918d06f7d66303c2
Sam <samuelkebbell@gmail.com>
2024-08-13 16:36:59 +0100

    updates

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -23,9 +23,8 @@ Player.prototype.getMarker = function () {
   return this.marker;
 }
 
+
 const player1 = new Player('player1', 'X');
 const player2 = new Player('player2', 'O');
 
 console.log(player1, player2)
-
-
