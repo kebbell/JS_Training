9f75e4c54082707bec41e33659869a95ed7f8f6f
Sam <samuelkebbell@gmail.com>
2024-08-13 14:02:54 +0100

    updates

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+}
+
+new Person ('Sam', 1997);
+
+console.log(Person);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
 const Player = function (name, marker) {
   this.name = name;
   this.marker = marker;
